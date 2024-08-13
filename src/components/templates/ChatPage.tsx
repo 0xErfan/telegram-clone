0f7245df54208d66ceab0988082cb7b37c90327f
Erfan <eftekharierfan5555555555@gmail.com>
2024-08-13 13:26:22 +0330

    add custom scrollbar & make the header sticky

--- src/components/templates/ChatPage.tsx
+++ src/components/templates/ChatPage.tsx
@@ -6,31 +6,33 @@ const ChatPage = () => {
     return (
         <div className="flex items-center bg-red-600 size-full ch:size-full h-screen overflow-hidden">
 
-            <div className="flex-1 bg-leftBarBg p-4 overflow-y-auto">
+            <div className="flex-1 bg-leftBarBg px-4 overflow-y-auto">
 
-                <div className="flex items-center gap-3 w-full text-white tex-[14px]">
-                    <Image
-                        className="cursor-pointer"
-                        src="/shapes/hamberger.svg"
-                        width={18}
-                        height={15}
-                        alt="hambergerMenu"
-                    />
-                    <p>Chats</p>
+                <div className="sticky top-0 bg-leftBarBg py-4 z-30">
+                    <div className="flex items-center gap-3 w-full text-white tex-[14px]">
+                        <Image
+                            className="cursor-pointer"
+                            src="/shapes/hamberger.svg"
+                            width={18}
+                            height={15}
+                            alt="hambergerMenu"
+                        />
+                        <p>Chats</p>
+                    </div>
+
+                    <div className="flex items-center justify-betweens rounded py-2 px-4 border-b-2 border-white/[78.6%] mt-4 bg-white/[6.05%] text-white/[78.6%]">
+                        <input
+                            className="bg-transparent flex-[20] outline-none"
+                            type="text"
+                            placeholder="Search"
+                        />
+                        <BiSearch className="flex-1 cursor-pointer size-[18px]" />
+                    </div>
                 </div>
 
-                <div className="flex items-center justify-betweens rounded py-2 px-4 border-b-2 border-white/[78.6%] mt-4 bg-white/[6.05%] text-white/[78.6%]">
-                    <input
-                        className="bg-transparent flex-[20] outline-none"
-                        type="text"
-                        placeholder="Search"
-                    />
-                    <BiSearch className="flex-1 cursor-pointer size-[18px]" />
-                </div>
-
-                <div className="flex flex-col mt-3">
+                <div className="flex flex-col h-[calc(100vh - 80px)] overflow-auto">
                     {
-                        Array(20).fill(0).map((_, index) => <ChatCard key={index} />)
+                        Array(5).fill(0).map((_, index) => <ChatCard key={index} />)
                     }
                 </div>
 
