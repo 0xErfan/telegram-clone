425c794df11043fdc83e6e4c4f324b8cc13d5a4a
Erfan <eftekharierfan5555555555@gmail.com>
2024-08-18 22:01:03 +0330

    add sign in logic and endpoint

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,28 @@
+import connectToDB from "@/db/db";
+import UserModel from "@/models/User";
+import { compare } from "bcrypt";
+import { tokenGenerator } from "@/utils";
+import { cookies } from "next/headers";
+
+export const POST = async (req: Request) => {
+
+    try {
+        await connectToDB()
+
+        const { payload, password } = await req.json()
+
+        const userData = await UserModel.findOne({ $or: [{ username: payload }, { phone: isNaN(payload) ? 0 : payload }] })
+        if (!userData) return Response.json({ message: 'No user exist with this username or password.' }, { status: 401 })
+
+        if (! await compare(password, userData.password)) return Response.json({ message: 'Incorrect username/phone or password' }, { status: 401 })
+
+        const token = tokenGenerator(userData.phone, 7)
+
+        cookies().set('token', token, { httpOnly: true, path: "/", maxAge: 60 * 60 * 24 * 6 })
+        return Response.json({ userData, message: 'You logged in successfully.' }, { status: 200 })
+
+    } catch (err) {
+        console.log(err)
+        return Response.json({ message: 'Unknown error, try later.' }, { status: 500 })
+    }
+}
--- src/components/templates/SignInForm.tsx
+++ src/components/templates/SignInForm.tsx
@@ -1,23 +1,49 @@
+import { showToast } from "@/utils";
+import useUserStore from "@/zustand/userStore";
 import { Button } from "@nextui-org/button"
 import { Input } from "@nextui-org/input"
+import axios from "axios";
 import { useState } from "react";
 
 const SignInForm = () => {
 
-    const [username, setUserName] = useState('');
+    const [payload, setPayload] = useState('');
     const [password, setPassword] = useState('')
 
+    const { setter, updater } = useUserStore(state => state)
+
+    const submitForm = async () => {
+
+        if (!payload.trim().length || !password.trim().length) return;
+
+        try {
+
+            const response = await axios.post('/api/auth/login', { payload, password })
+
+            if (response.status == 200) {
+                setter(response.data)
+                updater('isLogin', true)
+                showToast(true, 'You logged in successfully.')
+            }
+
+        } catch (error: any) { showToast(false, error.response.data.message, 3000) }
+    }
+
     return (
-        <div data-aos='zoom-out' className="flex w-full flex-col mt-10 space-y-6 ch:text-xl">
+        <div
+            data-aos='zoom-out'
+            className="flex w-full flex-col mt-10 space-y-6 ch:text-xl"
+            onKeyUp={e => e.key == 'Enter' && submitForm()}
+        >
 
             <Input
                 variant='bordered'
                 color="primary"
                 radius="sm"
-                label="UserName"
-                value={username}
-                onChange={e => setUserName(e.target.value)}
-                placeholder="Enter your username"
+                label="username/phone"
+                value={payload}
+                onChange={e => setPayload(e.target.value)}
+                placeholder="Enter your username/phone"
             />
 
             <Input
@@ -37,6 +63,7 @@ const SignInForm = () => {
                 radius="sm"
                 className="w-full"
                 variant="shadow"
+                onClick={submitForm}
             >
                 Sign in
             </Button>
