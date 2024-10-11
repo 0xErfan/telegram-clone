0b63f57e9805ef8d1fee7d97376631c1df851517
Erfan <eftekharierfan5555555555@gmail.com>
2024-10-11 19:13:21 +0330

    update env file

--- src/components/templates/LeftBar.tsx
+++ src/components/templates/LeftBar.tsx
@@ -17,7 +17,7 @@ const LeftBarMenu = lazy(() => import('@/components/templates/LeftBarMenu'))
 const SearchPage = lazy(() => import('@/components/templates/SearchPage'))
 const Modal = lazy(() => import('../modules/Modal'))
 
-const roomSocket = io(process.env?.NEXT_PUBLIC_BASE_PATH!, {
+const roomSocket = io('http://localhost:3001/', {
     autoConnect: true,
     reconnectionAttempts: Infinity,
     reconnectionDelay: 1000,
