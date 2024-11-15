8f80b3cd877e73a9752e7900d7d4c74811365a76
0xerfan <eftekharierfan5555555555@gmail.com>
2024-11-15 13:09:27 +0330

    fix message sender dynamic height

--- src/components/templates/MessageSender.tsx
+++ src/components/templates/MessageSender.tsx
@@ -1,6 +1,6 @@
 import { BsEmojiSmile } from "react-icons/bs";
 import { IoIosSend, IoMdClose } from "react-icons/io";
-import { ChangeEvent, ElementRef, lazy, useEffect, useRef, useState } from "react";
+import { ChangeEvent, ElementRef, lazy, Suspense, useEffect, useRef, useState } from "react";
 import { MdAttachFile, MdModeEditOutline, MdOutlineDone } from "react-icons/md";
 import { BsFillReplyFill } from "react-icons/bs";
 import useGlobalVariablesStore from "@/zustand/globalVariablesStore";
@@ -8,6 +8,7 @@ import useUserStore from "@/zustand/userStore";
 import useSockets from "@/zustand/useSockets";
 import { MessageModel } from "@/@types/data.t";
 import VoiceMessageRecorder from "./VoiceMessageRecorder";
+
 const EmojiPicker = lazy(() => import('emoji-picker-react'))
 
 interface Props {
@@ -27,6 +28,7 @@ const MessageSender = ({ replayData, editData, closeReplay, closeEdit }: Props) 
     const msgSenderRef = useRef<ElementRef<'section'>>(null)
     const [isMuted, setIsMuted] = useState(false)
     const [isEmojiOpen, setIsEmojiOpen] = useState(false)
+    const [msgSenderHeight, setMsgSenderHeight] = useState<`${number}px`>('0px')
 
     const selectedRoom = useGlobalVariablesStore(state => state?.selectedRoom)
     const userRooms = useUserStore(state => state.rooms)
@@ -35,39 +37,22 @@ const MessageSender = ({ replayData, editData, closeReplay, closeEdit }: Props) 
 
     const _id = selectedRoom?._id;
 
-    const msgSenderHeight = `${msgSenderRef?.current?.clientHeight}px`
-
-    useEffect(() => {
-        setText(editData?.message || '')
-    }, [editData?.message])
-
-    useEffect(() => {
-
-        const draftMessage = localStorage.getItem(_id!)
-
-        draftMessage?.length && setText(draftMessage)
-        draftMsg = draftMessage?.length ? draftMessage : ''
-
-        return () => {
-            localStorage.setItem(_id!, draftMsg || '')
-            setText('')
+    const updateHeight = () => {
+        if (msgSenderRef.current) {
+            setMsgSenderHeight(`${msgSenderRef.current.clientHeight}px`);
         }
-
-    }, [_id])
-
-    useEffect(() => resizeTextArea(), [text, editData?._id])
-
-    useEffect(() => {
-        if (replayData?._id || editData?._id) inputRef.current?.focus()
-    }, [replayData?._id, editData?._id])
+    };
 
     const cleanUpAfterSendingMsg = () => {
+
         if (inputRef?.current) { inputRef.current.style.height = '22px' }
+        updateHeight()
         closeReplay()
         closeEdit()
         setText('')
         draftMsg = ''
         localStorage.removeItem(_id!)
+
     }
 
     const sendMessage = () => {
@@ -140,13 +125,55 @@ const MessageSender = ({ replayData, editData, closeReplay, closeEdit }: Props) 
         }
     };
 
+    useEffect(() => {
+
+        const timeout = setTimeout(updateHeight, 10);
+
+        return () => clearTimeout(timeout);
+
+    }, [editData?._id, selectedRoom?._id]);
+
+    useEffect(() => {
+        setText(editData?.message || '')
+    }, [editData?.message])
+
+    useEffect(() => {
+
+        const draftMessage = localStorage.getItem(_id!)
+
+        draftMessage?.length && setText(draftMessage)
+        draftMsg = draftMessage?.length ? draftMessage : ''
+
+        return () => {
+            localStorage.setItem(_id!, draftMsg || '')
+            setText('')
+        }
+
+    }, [_id])
+
+    useEffect(() => resizeTextArea(), [text])
+
+    useEffect(() => {
+        if (replayData?._id || editData?._id) inputRef.current?.focus()
+    }, [replayData?._id, editData?._id])
+
     return (
         <section ref={msgSenderRef} className='sticky -mx-4 md:mx-0 bg-chatBg z-[999999] bottom-0 md:pb-3 inset-x-0'
         >
 
-            <div style={{ bottom: msgSenderHeight }} className={`${(replayData?._id || editData?._id) ? 'opacity-100 h-[50px] pb-1' : 'opacity-0 h-0'} flex flex-row-reverse justify-between duration-200 transition-all items-center gap-3 px-4 line-clamp-1 overflow-ellipsis absolute rounded-t-xl bg-white/[5.12%] inset-x-0 z-40`}>
+            <div
+                style={{ bottom: msgSenderHeight }}
+                className={`${(replayData?._id || editData?._id) ? 'opacity-100 h-[50px] pb-1' : 'opacity-0 h-0'} flex flex-row-reverse justify-between duration-200 transition-all items-center gap-3 px-4 line-clamp-1 overflow-ellipsis absolute rounded-t-xl bg-white/[5.12%] inset-x-0 z-40`}
+            >
 
-                <IoMdClose onClick={() => { closeReplay(), closeEdit() }} className="size-7 shrink-0 transition-all cursor-pointer active:bg-inherit active:rounded-full p-1" />
+                <IoMdClose
+                    onClick={() => {
+                        closeReplay()
+                        closeEdit()
+                        if (inputRef.current) inputRef.current.style.height = '24px'
+                    }}
+                    className="size-7 shrink-0 transition-all cursor-pointer active:bg-inherit active:rounded-full p-1"
+                />
 
                 <div className="flex items-center gap-4 line-clamp-1 overflow-ellipsis">
 
@@ -171,25 +198,33 @@ const MessageSender = ({ replayData, editData, closeReplay, closeEdit }: Props) 
                         </p>
 
                     </div>
+
                 </div>
 
             </div>
 
             <span style={{ bottom: msgSenderHeight }} className={`${(replayData?._id || editData?._id) ? 'opacity-100 h-[50px] pb-1' : 'opacity-0 h-0'} duration-200 transition-all border-b border-white/5 z-30 absolute inset-x-0 bg-inherit`}></span>
 
-            <EmojiPicker
-                open={isEmojiOpen}
-                //@ts-expect-error
-                theme="dark"
-                className="absolute left-3 w-32 bottom-16"
-                style={{ position: 'absolute' }}
-                //@ts-expect-error
-                previewConfig={{ defaultCaption: false, defaultEmoji: false, showPreview: false }}
-                skinTonesDisabled={true}
-                searchDisabled={true}
-                lazyLoadEmojis={true}
-                onEmojiClick={selectEmoji}
-            />
+            {
+                isEmojiOpen
+                    ?
+                    <Suspense>
+                        <EmojiPicker
+                            open={isEmojiOpen}
+                            //@ts-expect-error
+                            theme="dark"
+                            className="absolute left-3 w-32 bottom-16"
+                            style={{ position: 'absolute' }}
+                            //@ts-expect-error
+                            previewConfig={{ defaultCaption: false, defaultEmoji: false, showPreview: false }}
+                            skinTonesDisabled={true}
+                            searchDisabled={true}
+                            lazyLoadEmojis={true}
+                            onEmojiClick={selectEmoji}
+                        />
+                    </Suspense>
+                    : null
+            }
 
             <div className='flex items-end relative w-full md:px-2 px-6 ch:w-full md:gap-1 gap-3 bg-white/[5.12%] min-h-[53px] h-auto rounded pb-[15px] mt-2'>
 
@@ -245,7 +280,7 @@ const MessageSender = ({ replayData, editData, closeReplay, closeEdit }: Props) 
                         :
                         <div
                             onClick={() => setIsMuted(prev => !prev)}
-                            className="absolute size-full cursor-pointer pt-3 mb-[14px] text-center">{isMuted ? 'Unmute' : 'Mute'}
+                            className="absolute cursor-pointer flex items-center justify-center pt-3 text-center">{isMuted ? 'Unmute' : 'Mute'}
                         </div>
                 }
 
