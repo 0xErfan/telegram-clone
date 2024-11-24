a7ca1db81aa04f0a1b41134563b4752d571b563b
0xerfan <eftekharierfan5555555555@gmail.com>
2024-11-24 22:48:19 +0330

    update user model schema & add socket event for last message id updater

--- src/models/Room.js
+++ src/models/Room.js
@@ -6,7 +6,7 @@ const RoomSchema = new Schema({
     type: { type: String, enum: ['group', 'private', 'chanel'], required: true },
     admins: [{ type: Schema.ObjectId, ref: 'User', required: true }],
     participants: [{ type: Schema.ObjectId, ref: 'User', required: true }],
-    creator: { type: Schema.ObjectId, ref: 'User'},
+    creator: { type: Schema.ObjectId, ref: 'User' },
     medias: [{ type: Schema.ObjectId, ref: 'Media', required: true }],
     messages: [{ type: Schema.ObjectId, ref: 'Message', required: true }],
     locations: [{ type: Schema.ObjectId, ref: 'Location', required: true }],
